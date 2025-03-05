a2437dbdc9ab896db6bee0aabfb5c40ad884b2ca
giuseppeps <giuseppe_000@hotmail.com>
2025-03-05 18:34:03 -0300

    feat: add custom font asset

--- lib/theme/texts.dart
+++ lib/theme/texts.dart
@@ -3,35 +3,35 @@ import 'package:flutter_dev_test/theme/custom_colors.dart';
 
 const titleText20 = TextStyle(
   fontSize: 20,
-  fontFamily: 'Plus Jakarta Sans',
+  fontFamily: 'PlusJakartaSans',
   fontWeight: FontWeight.w500,
   color: CustomColors.primary,
 );
 
 const titleText18 = TextStyle(
   fontSize: 18,
-  fontFamily: 'Plus Jakarta Sans',
+  fontFamily: 'PlusJakartaSans',
   fontWeight: FontWeight.w500,
   color: CustomColors.primary,
 );
 
 const titleText16 = TextStyle(
   fontSize: 16,
-  fontFamily: 'Plus Jakarta Sans',
+  fontFamily: 'PlusJakartaSans',
   fontWeight: FontWeight.w500,
   color: CustomColors.primary,
 );
 
 const titleText14 = TextStyle(
   fontSize: 14,
-  fontFamily: 'Plus Jakarta Sans',
+  fontFamily: 'PlusJakartaSans',
   fontWeight: FontWeight.w500,
   color: CustomColors.primary,
 );
 
 const titleText12 = TextStyle(
   fontSize: 12,
-  fontFamily: 'Plus Jakarta Sans',
+  fontFamily: 'PlusJakartaSans',
   fontWeight: FontWeight.w500,
   color: CustomColors.primary,
 );
